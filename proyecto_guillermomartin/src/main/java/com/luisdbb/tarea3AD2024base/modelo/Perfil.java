030a52a6ce5fd721b719998972a977edde67584b
guillermomf38 <guillermomf38@educastur.es>
2026-04-23 13:26:06 +0200

    commit

--- proyecto_guillermomartin/src/main/java/com/luisdbb/tarea3AD2024base/modelo/Perfil.java
+++ proyecto_guillermomartin/src/main/java/com/luisdbb/tarea3AD2024base/modelo/Perfil.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
  *Clase Perfil.java
  * 
@@ -13,19 +13,3 @@ package com.luisdbb.tarea3AD2024base.modelo;
 public enum Perfil {
 ADMINISTRADOR, ESTUDIANTE, PROFESORADO, TUTOREMPRESA
 }
-=======
-/**
- *Clase Perfil.java
- * 
- *@author Guillermo Martin Fueyo
- *@version 1.0
- */
-
-
-
-package com.luisdbb.tarea3AD2024base.modelo;
-
-public enum Perfil {
-ADMINISTRADOR, ESTUDIANTE, PROFESORADO, TUTOREMPRESA
-}
->>>>>>> branch 'main' of https://github.com/guillermomf38/PROYECTO_MARTINFUEYO_GUILLERMO.git
